a9489a1b336bfba02a598eab21468fa34832d3f7
Tiago Ventura <tfj.ventura@gmail.com>
2022-01-22 02:22:07 +0100

    Alteração das variaveis de ambiente

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { OrderPayload } from "./Orders/types";
 
-let API_URL = "http://localhost:8080";
+let API_URL = process.env.REACT_APP_API_URL;
 let mapboxToken = process.env.REACT_APP_ACCESS_TOKEN_MAP_BOX;
 export function fetchProducts (){
     return axios(`${API_URL}/products`)
