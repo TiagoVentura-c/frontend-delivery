f02fe88e0549953cc735b3751cd5209183e8cdea
Tiago Ventura <tfj.ventura@gmail.com>
2022-01-22 01:55:20 +0100

    Alteração Footer

--- src/Footer/index.tsx
+++ src/Footer/index.tsx
@@ -10,9 +10,9 @@ function Footer(){
         <footer className='main-footer'>
             Todos os direitos reservados
             <div className='footer-icons'>
-                <a href="" target='_new'><YoutubeIcon/></a>
-                <a href="" target='_new'><InstagramIcon/></a>
-                <a href="" target='_new'><LinkedinIcon/></a>
+                <a href="youtube.com" target='_new'><YoutubeIcon/></a>
+                <a href="youtube.com" target='_new'><InstagramIcon/></a>
+                <a href="youtube.com" target='_new'><LinkedinIcon/></a>
             </div>
         </footer>
     )
